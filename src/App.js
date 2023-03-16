5b59f645c7b0064582758d0e32d5efa67c474767
TeonaZav <teona.zavrashvili@gmail.com>
2023-03-16 16:19:42 +0400

    feat: add initial fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,27 @@
+import React, { useState, useEffect } from "react";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(100);
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const fetchUsers = async () => {
+    let url = `${BASE_URL}/${currentPage}/${pageSize}`;
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setUsers(data.list);
+      console.log(data.list);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchUsers();
+  }, []);
   return <div className="App"></div>;
 }
 
